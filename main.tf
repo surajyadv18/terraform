4ef28409462d8fcb6074f1262885aab7b2469d46
surajyadv18 <suraj.yadav@enveu.com>
2024-06-23 20:16:45 +0530

    added main.tf

--- main.tf
+++ main.tf
@@ -14,10 +14,14 @@ provider "aws" {
 resource "aws_instance" "S1" {
   ami           = "ami-0e001c9271cf7f3b9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [data.aws_security_group.terraform_sg.id]
 }
 
-resource "aws_security_group" "terraform_sg" {
-  name        = "security group using terraform"
-  description = "security group using terraform"
-  vpc_id      = "vpc-0491bafa28c55262c"
+data "aws_security_group" "terraform_sg" {
+  filter {
+    name   = "security group using terraform"
+    values = ["security group using terraform"]
+  }
+
+  vpc_id = "vpc-0491bafa28c55262c"
 }
